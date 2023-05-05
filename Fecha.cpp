9f3c6f716c4cde4eeab6a53bc567b8a2863c4c7f
Deus Ex Umbra <Deus Ex Umbra@DESKTOP-81K5OR1>
2023-05-05 18:06:44 -0400

    Proyecto totalmente acabado... Hasta que encuentre más errores.

--- Fecha.cpp
+++ Fecha.cpp
@@ -11,8 +11,24 @@ int main(int argv, char** argc) {
 	cout << "Ingrese una fecha: "; getline(cin, fecha);
 	cout << "----------------------------------------------------\n";
 	while (!fecha_i.fecha_correcta(fecha)) {
-		cout << "Ingrese una fecha correcta: "; cin.ignore();  getline(cin, fecha);
+		switch (fecha_i.get_tipo_error()) {
+		case Fecha_a_Dia::error::DIA:
+			cout << "Error: Día fuera de rango.\n"; break;
+		case Fecha_a_Dia::error::MES:
+			cout << "Error: Mes fuera de rango.\n"; break;
+		case Fecha_a_Dia::error::ANIO:
+			cout << "Error: Año fuera de rango.\n"; break;
+		case Fecha_a_Dia::error::DIA_30:
+			cout << "Error: Día de " << fecha_i.get_mes_ingresado() << " es mayor a 30 días.\n"; break;
+		case Fecha_a_Dia::error::DIA_BIS:
+			cout << "Error: Día de " << fecha_i.get_mes_ingresado() << " es mayor a 28 o 29 días.\n"; break;
+		case Fecha_a_Dia::error::ANIO_M:
+			cout << "Error: Año ingresado es mayor a año actual.\n"; break;
+		}
+		cout << "Ingrese una fecha correcta: "; getline(cin, fecha);
 	}
 	fecha_i.set_fecha_ingresada(fecha);
+	cout << "La fecha ingresada de la fecha actual se separa por " << fecha_i.get_cantidad_de_dias() << " días.\n";
+	system("pause");
 	return 0;
 }
--- Fecha_a_Dia.h
+++ Fecha_a_Dia.h
@@ -1,5 +1,6 @@
 #pragma once
 #include<iostream>
+#include<vector>
 #include<time.h>
 #include<string>
 using namespace std;
@@ -7,7 +8,7 @@ class Fecha_a_Dia
 {
 private:
 	string fecha_actual, fecha_in, dia_a, mes_a, anio_a, dia, mes, anio, hora_a, minuto_a, segundo_a;
-	int cantidad_dias;
+	int cantidad_de_dias;
 	void set_fecha_actual() {
 		time_t f;
 		struct tm fa = {0};
@@ -26,10 +27,12 @@ private:
 		if (!es_un_numero(_fecha_c))
 		{
 			int pos_no_digit1 = _fecha_c.find_first_not_of("0123456789");
-			int pos_no_digit2 = _fecha_c.find_first_not_of("0123456789", pos_no_digit1 + 1);
+			int pos_digit1 = _fecha_c.find_first_of("0123456789", pos_no_digit1 + 1);
+			int pos_no_digit2 = _fecha_c.find_first_not_of("0123456789", pos_digit1 + 1);
+			int pos_digit2 = _fecha_c.find_first_of("0123456789", pos_no_digit2 + 1);
 			dia = _fecha_c.substr(0, pos_no_digit1);
-			mes = _fecha_c.substr(pos_no_digit1 + 1, pos_no_digit2 - pos_no_digit1 - 1);
-			anio = _fecha_c.substr(pos_no_digit2 + 1);
+			mes = _fecha_c.substr(pos_digit1, pos_no_digit2 - pos_digit1);
+			anio = _fecha_c.substr(pos_digit2);
 		}else {
 			dia = _fecha_c.substr(0, 2);
 			mes = _fecha_c.substr(2, 2);
@@ -48,30 +51,56 @@ private:
 	bool anio_bisiesto(string _anio_c) {
 		return (stoi(_anio_c) % 100 == 0) ? (stoi(_anio_c) % 400 == 0) : (stoi(_anio_c) % 4 == 0);
 	}
+	int get_cantidad_anio_bisiesto(int anio) {
+		return (anio / 4 - anio / 100 + anio / 400);
+	}
+	int suma_meses(vector <int> _meses, int _cantidad_mes) {
+		int suma = 0;
+		int index = 0;
+		for (int indice : _meses) {
+			suma += indice; index++; if (index >= _cantidad_mes) { break; }
+		}
+		cout << suma << "\n";
+		return suma;
+	}
 public:
-	Fecha_a_Dia() {
+	Fecha_a_Dia() : cantidad_de_dias(0){
 		set_fecha_actual();
 	}
 	bool fecha_correcta(string _fecha_c) {
-		set_dia_mes_anio(_fecha_c);
-		if ((stoi(dia) <= 0) || (stoi(dia) >= 31)) { return false; }
-		if ((stoi(mes) <= 0) || stoi(mes) > 12) { return false; }
-		if (stoi(anio) <= 0) { return false; }
-		if (((stoi(dia) > 30) && (stoi(mes) == 4)) || ((stoi(dia) > 30) && (stoi(mes) == 6)) || ((stoi(dia) > 30) && (stoi(mes) == 9)) || ((stoi(dia) > 30) && (stoi(mes) == 11))) { return false; }
-		if ((anio_bisiesto(anio) && stoi(dia) > 29) || (!anio_bisiesto(anio) && stoi(dia) > 28)) { return false; }
-		return true;
+		try {
+			set_dia_mes_anio(_fecha_c);
+			if ((stoi(dia) <= 0) || (stoi(dia) >= 31)) { tipo_error = DIA; return false; }
+			if ((stoi(mes) <= 0) || stoi(mes) > 12) { tipo_error = MES; return false; }
+			if (stoi(anio) < 0) { tipo_error = ANIO; return false; }
+			if (((stoi(dia) > 30) && (stoi(mes) == 4)) || ((stoi(dia) > 30) && (stoi(mes) == 6)) || ((stoi(dia) > 30) && (stoi(mes) == 9)) || ((stoi(dia) > 30) && (stoi(mes) == 11))) { tipo_error = DIA_30; return false; }
+			if ((anio_bisiesto(anio) && stoi(dia) > 29) || (!anio_bisiesto(anio) && stoi(dia) > 28)) { tipo_error = DIA_BIS; return false; }
+			if (stoi(anio) > stoi(anio_a)) { tipo_error = ANIO_M; return false; }
+			return true;
+		} catch (const exception& e) {
+			cout << "Error: Usted ha ingresado una fecha en un formato no aceptable. " << e.what() << "\n"; return false;
+		}
 	}
 	void set_fecha_ingresada(string _fecha_ingresar) {
 		set_dia_mes_anio(_fecha_ingresar);
 		cout << "La fecha es correcta.\n";
 		fecha_in = dia + "/" + mes + "/" + anio;
-		cout << "La fecha es: " << fecha_in;
+		cout << "La fecha es: " << fecha_in << "\n";
 	}
-	string get_fecha_ingresada() {
-		return fecha_in;
+	string get_dia_ingresado() { return dia; }
+	string get_mes_ingresado() { return mes; }
+	string get_anio_ingresado() { return anio; }
+	string get_fecha_ingresada() { return fecha_in; }
+	string get_fecha_actual() { return fecha_actual; }
+	int get_cantidad_de_dias() {
+		vector <int> meses = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		int dias_a = stoi(dia_a) + suma_meses(meses, stoi(mes_a)) + stoi(anio_a) * 365 + get_cantidad_anio_bisiesto(stoi(anio_a));
+		int dias = stoi(dia) + suma_meses(meses, stoi(mes)) + stoi(anio) * 365 + get_cantidad_anio_bisiesto(stoi(anio));
+		cantidad_de_dias = dias_a - dias;
+		return cantidad_de_dias;
 	}
-	string get_fecha_actual() {
-		return fecha_actual;
-	}
+	enum error { NINGUNO, DIA, MES, ANIO, DIA_30, DIA_BIS, ANIO_M };
+	error tipo_error = NINGUNO;
+	int get_tipo_error() { return tipo_error; }
 };
 
